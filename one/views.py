8dd2fbd95a1dd955753c061985565018abece459
ManharCK <manhargurukkal2@gmail.com>
2026-06-07 18:26:59 +0530

    updated

--- one/views.py
+++ one/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
 from rest_framework .views import APIView
-from rest_framework import response
+# from rest_framework import response
 from rest_framework import status
+from rest_framework .response import Response
 
 from .models import Hotel,Food
 from .serializers import Hotelserializer,Foodserializer
@@ -15,20 +16,51 @@ class hotelapi(APIView):
 
         serializer=Hotelserializer(hotel,many=True)
 
-        return response(serializer.data)
+        return Response(serializer.data)
     def post(self,request):
         serializer=Hotelserializer(data=request.data)
 
-        if serializer.is_valid:
+        if serializer.is_valid():
             serializer.save()
 
-            return response(serializer.data,status=status.HTTP_201_CREATED)
-        return response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     
 
 
 class hoteldetailed(APIView):
-    pass
+
+    def get(self,request,pk):
+        hotel=Hotel.objects.get(id=pk)
+
+        serializer=Hotelserializer(hotel)
+        return Response(serializer.data)
+    
+    def put(self,request,pk):
+
+        hotel=hotel.objects.get(id=pk)
+
+        serializer=Hotelserializer(hotel,data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self,request,pk):
+        htl=Hotel.objects.get(id=pk)
+        htl.delete()
+
+        return Response('deletyed')
+        
+    
+ 
+
+
+
+        pass
+    
     
 
 
