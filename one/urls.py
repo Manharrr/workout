58af5bcba3b7855ee8d481ebe253da5f900dd52d
ManharCK <manhargurukkal2@gmail.com>
2026-06-11 10:56:35 +0530

    updated

--- one/urls.py
+++ one/urls.py
@@ -4,8 +4,8 @@ from . models import Food,Hotel
 from .views import hotelapi,hoteldetailed
 
 
-urlpatterns = [
-    path('hotel/',hotelapi.as_view()),
-    path('hotel/<int:pk>/',hoteldetailed.as_view()),
+# urlpatterns = [
+#     path('hotel/',hotelapi.as_view()),
+#     path('hotel/<int:pk>/',hoteldetailed.as_view()),
     
-]
+# ]
